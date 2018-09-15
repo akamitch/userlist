026733a7f72f34ac11b584884582045aedf6c75a
Mitch <mitch@aka-root.com>
2018-09-15 04:08:28 +0300

    добавил поля в User добавил .gitignore

--- src/main/java/com/example/userlist/model/User.java
+++ src/main/java/com/example/userlist/model/User.java
@@ -3,13 +3,82 @@ package com.example.userlist.model;
 public class User {
 
     private String name;
+    private Long id;
+    private String username;
+    private String password;
+    private boolean accountNonExpired;
+    private boolean accountNonLocked;
+    private boolean credentialsNonExpired;
+    private boolean enabled;
 
     public User(String name) {
         this.name = name;
+        accountNonExpired = true;
+        accountNonLocked = true;
+        credentialsNonExpired = true;
+        enabled = true;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public boolean isAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public boolean isAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public boolean isCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
     }
 
     public User() {
     }
+
+
 
     public String getName() {
         return this.name;
@@ -19,9 +88,9 @@ public class User {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        return "User name is: " + getName();
-    }
+//    @Override
+//    public String toString() {
+//        return "User name is: " + getName();
+//    }
 
 }
